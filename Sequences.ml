d6ab3ae80fd7d14a37676249be50b77f5e6aa9cd
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2024-07-04 14:34:27 +0200

    Added more functions and types to Sequences.

--- Sequences.ml
+++ Sequences.ml
@@ -1,6 +1,36 @@
+[@@@ocaml.warnings "-32"] (* "unused-value-declaration" --- But how? *)
+
 type        'a    nseq = 'a * 'a list
 type ('a,'sep) nsepseq = 'a * ('sep * 'a) list
 type ('a,'sep)  sepseq = ('a,'sep) nsepseq option
+
+type ('a,'sep) nsep_or_term = [
+  `Sep  of ('a,'sep) nsepseq
+| `Term of ('a * 'sep) nseq
+]
+
+type ('a,'sep) sep_or_term = ('a,'sep) nsep_or_term option
+
+type ('a,'sep) nsep_or_pref = [
+  `Sep  of ('a,'sep) nsepseq
+| `Pref of ('sep * 'a) nseq
+]
+
+(* Utilities *)
+
+let (<@) f g x = f (g x)
+
+(* Creation *)
+
+let nseq_create    x l = x, l
+let nsepseq_create x l = x, l
+let sepseq_create  x l = Some (x, l)
+
+(* Singletons *)
+
+let nseq_singleton    x = nseq_create    x []
+let nsepseq_singleton x = nsepseq_create x []
+let sepseq_singleton  x = sepseq_create  x []
 
 (* Consing *)
 
@@ -11,24 +41,61 @@ let sepseq_cons x sep = function
           None -> x, []
 | Some (hd,tl) -> x, (sep,hd)::tl
 
+let nsep_or_term_cons x sep = function
+  `Sep  s -> `Sep (nsepseq_cons x sep s)
+| `Term s -> `Term (nseq_cons (x,sep) s)
+
+let sep_or_term_cons x sep = function
+  None   -> `Term (nseq_singleton (x,sep))
+| Some s -> nsep_or_term_cons x sep s
+
+let nsep_or_pref_cons x sep = function
+  `Sep  s -> `Sep (nsepseq_cons x sep s)
+| `Pref s -> `Pref (nseq_cons (sep,x) s)
+
 (* Rightwards iterators *)
 
-let nseq_foldl f a (hd,tl) = List.fold_left f a (hd::tl)
+let nseq_foldl f init (hd,tl) = List.fold_left f init (hd::tl)
 
-let nsepseq_foldl f a (hd,tl) =
-  List.fold_left (fun a (_,e) -> f a e) (f a hd) tl
+let nsepseq_foldl f init (hd,tl) =
+  List.fold_left (fun a (_,e) -> f a e) (f init hd) tl
 
-let sepseq_foldl f a = function
-    None -> a
-| Some s -> nsepseq_foldl f a s
+let sepseq_foldl f init = function
+    None -> init
+| Some s -> nsepseq_foldl f init s
+
+let nsep_or_term_foldl f init = function
+  `Sep  s -> nsepseq_foldl f init s
+| `Term s -> nseq_foldl (fun a -> f a <@ fst) init s
+
+let sep_or_term_foldl f init = function
+  None   -> init
+| Some s -> nsep_or_term_foldl f init s
+
+let nsep_or_pref_foldl f init = function
+  `Sep  s -> nsepseq_foldl f init s
+| `Pref s -> nseq_foldl (fun a -> f a <@ snd) init s
+
 
 let nseq_iter f (hd,tl) = List.iter f (hd::tl)
 
-let nsepseq_iter f (hd,tl) = f hd; List.iter (fun (_,x) -> f x) tl
+let nsepseq_iter f (hd,tl) = f hd; List.iter (f <@ snd) tl
 
 let sepseq_iter f = function
     None -> ()
 | Some s -> nsepseq_iter f s
+
+let nsep_or_term_iter f = function
+  `Sep  s -> nsepseq_iter f s
+| `Term s -> nseq_iter (f <@ fst) s
+
+let sep_or_term_iter f = function
+  None   -> ()
+| Some s -> nsep_or_term_iter f s
+
+let nsep_or_pref_iter f = function
+  `Sep  s -> nsepseq_iter f s
+| `Pref s -> nseq_iter (f <@ snd) s
 
 (* Reversing *)
 
@@ -50,16 +117,40 @@ let sepseq_rev = function
       None -> None
 | Some seq -> Some (nsepseq_rev seq)
 
+let nsep_or_term_rev = function
+  `Sep  s -> `Sep (nsepseq_rev s)
+| `Term s -> `Term (nseq_rev s)
+
+let sep_or_term_rev = function
+  None   -> None
+| Some s -> Some (nsep_or_term_rev s)
+
+let nsep_or_pref_rev = function
+  `Sep  s -> `Sep (nsepseq_rev s)
+| `Pref s -> `Pref (nseq_rev s)
+
 (* Leftwards iterators *)
 
 let nseq_foldr f (hd,tl) = List.fold_right f (hd::tl)
 
-let nsepseq_foldr f (hd,tl) a =
-  f hd (List.fold_right (fun (_,x) -> f x) tl a)
+let nsepseq_foldr f (hd,tl) init =
+  f hd (List.fold_right (f <@ snd) tl init)
 
 let sepseq_foldr f = function
-    None -> fun a -> a
+    None -> fun init -> init
 | Some s -> nsepseq_foldr f s
+
+let nsep_or_term_foldr f = function
+  `Sep  s -> nsepseq_foldr f s
+| `Term s -> fun init -> nseq_foldr (f <@ fst) s init
+
+let sep_or_term_foldr f = function
+  None   -> fun i -> i
+| Some s -> nsep_or_term_foldr f s
+
+let nsep_or_pref_foldr f = function
+  `Sep  s -> nsepseq_foldr f s
+| `Pref s -> fun init -> nseq_foldr (f <@ snd) s init
 
 (* Maps *)
 
@@ -72,6 +163,33 @@ let sepseq_map f = function
       None -> None
 | Some seq -> Some (nsepseq_map f seq)
 
+let nsep_or_term_map f = function
+  `Sep  s -> `Sep (nsepseq_map f s)
+| `Term s -> `Term (nseq_map (fun (x, term) -> (f x, term)) s)
+
+let sep_or_term_map f = function
+  None   -> None
+| Some s -> Some (nsep_or_term_map f s)
+
+let nsep_or_pref_map f = function
+  `Sep  s -> `Sep (nsepseq_map f s)
+| `Pref s -> `Pref (nseq_map (fun (pref, x) -> (pref, f x)) s)
+
+(* Conversions to non-empty lists *)
+
+let nsepseq_to_nseq (x, l) = nseq_create x (List.map snd l)
+
+let nsep_or_term_to_nseq = function
+  `Sep  s -> nsepseq_to_nseq s
+| `Term s -> nseq_map fst s
+
+let nsep_or_pref_to_nseq = function
+  `Sep  s -> nsepseq_to_nseq s
+| `Pref s -> nseq_map snd s
+
+let nseq_to_nsepseq sep (hd,tl) =
+  nsepseq_create hd @@ List.map (fun x -> (sep,x)) tl
+
 (* Conversions to lists *)
 
 let nseq_to_list (x,y) = x::y
@@ -81,3 +199,59 @@ let nsepseq_to_list (x,y) = x :: List.map snd y
 let sepseq_to_list = function
     None -> []
 | Some s -> nsepseq_to_list s
+
+let nsep_or_term_to_list = function
+  `Sep  s -> nsepseq_to_list s
+| `Term s -> List.map fst (nseq_to_list s)
+
+let sep_or_term_to_list = function
+  None     -> []
+| Some seq -> nsep_or_term_to_list seq
+
+let nsep_or_pref_to_list = function
+  `Sep s      -> nsepseq_to_list s
+| `Pref (x,l) -> List.map snd (x::l)
+
+(* Conversions from lists *)
+
+let list_to_sepseq (lst : 'a list) (sep : 's) : ('a, 's) sepseq =
+  match lst with
+    [] -> None
+  | hd :: tl -> Some (hd, List.map (fun e -> sep, e) tl)
+
+let sep_or_term_of_list sep sep_or_term lst : ('a,'sep) sep_or_term =
+  match lst with
+    [] -> None
+  | x :: l ->
+      match sep_or_term with
+        `Sep -> Some (`Sep (nseq_to_nsepseq sep (nseq_create x l)))
+      | `Term -> let list = List.map (fun x -> (x,sep)) l
+                 in Some (`Term (nseq_create (x,sep) list))
+
+(* Conversions to JSON *)
+
+type json = Yojson.Safe.t
+
+let yojson_of_nseq f (s : 'a nseq) =
+  `List (nseq_to_list @@ nseq_map f s)
+
+let yojson_of_nsepseq f _ (s : ('a, 'sep) nsepseq) =
+  `List (nsepseq_to_list @@ nsepseq_map f s)
+
+let yojson_of_sepseq f _ (s : ('a, 'sep) sepseq) =
+  `List (sepseq_to_list @@ sepseq_map f s)
+
+let yojson_of_sep_or_term f _ (s : ('a, 'sep) sep_or_term) =
+  `List (sep_or_term_to_list @@ sep_or_term_map f s)
+
+let yojson_of_nsep_or_term f _ (s : ('a, 'sep) nsep_or_term) =
+  `List (nsep_or_term_to_list @@ nsep_or_term_map f s)
+
+let yojson_of_nsep_or_pref f _ (s : ('a, 'sep) nsep_or_pref) =
+  `List (nsep_or_pref_to_list @@ nsep_or_pref_map f s)
+
+(* Map and concatenate lists *)
+
+let    nseq_concat_map f s = List.concat_map f (nseq_to_list    s)
+let  sepseq_concat_map f s = List.concat_map f (sepseq_to_list  s)
+let nsepseq_concat_map f s = List.concat_map f (nsepseq_to_list s)
--- Sequences.mli
+++ Sequences.mli
@@ -6,11 +6,44 @@ type           'a nseq = 'a * 'a list
 type ('a,'sep) nsepseq = 'a * ('sep * 'a) list
 type ('a,'sep)  sepseq = ('a,'sep) nsepseq option
 
+type ('a,'sep) nsep_or_term = [
+  `Sep  of ('a,'sep) nsepseq
+| `Term of ('a * 'sep) nseq
+]
+
+type ('a,'sep) sep_or_term = ('a,'sep) nsep_or_term option
+
+type ('a,'sep) nsep_or_pref = [
+  `Sep  of ('a,'sep) nsepseq
+| `Pref of ('sep * 'a) nseq
+]
+
+(* Creation *)
+
+val nseq_create    : 'a ->          'a list ->           'a nseq
+val nsepseq_create : 'a -> ('sep * 'a) list -> ('a,'sep) nsepseq
+val sepseq_create  : 'a -> ('sep * 'a) list -> ('a,'sep)  sepseq
+
+(* Singletons *)
+
+val nseq_singleton    : 'a -> 'a nseq
+val nsepseq_singleton : 'a -> ('a,'sep) nsepseq
+val sepseq_singleton  : 'a -> ('a,'sep) sepseq
+
 (* Consing *)
 
 val nseq_cons    : 'a -> 'a nseq -> 'a nseq
 val nsepseq_cons : 'a -> 'sep -> ('a,'sep) nsepseq -> ('a,'sep) nsepseq
 val sepseq_cons  : 'a -> 'sep -> ('a,'sep)  sepseq -> ('a,'sep) nsepseq
+
+val nsep_or_term_cons :
+  'a -> 'sep -> ('a,'sep) nsep_or_term -> ('a,'sep) nsep_or_term
+
+val sep_or_term_cons :
+  'a -> 'sep -> ('a,'sep) sep_or_term -> ('a,'sep) nsep_or_term
+
+val nsep_or_pref_cons :
+  'a -> 'sep -> ('a,'sep) nsep_or_pref -> ('a,'sep) nsep_or_pref
 
 (* Reversing *)
 
@@ -18,30 +51,110 @@ val nseq_rev    : 'a nseq -> 'a nseq
 val nsepseq_rev : ('a,'sep) nsepseq -> ('a,'sep) nsepseq
 val sepseq_rev  : ('a,'sep)  sepseq -> ('a,'sep)  sepseq
 
+val nsep_or_term_rev : ('a,'sep) nsep_or_term -> ('a,'sep) nsep_or_term
+val sep_or_term_rev  : ('a,'sep)  sep_or_term -> ('a,'sep)  sep_or_term
+val nsep_or_pref_rev : ('a,'sep) nsep_or_pref -> ('a,'sep) nsep_or_pref
+
+
 (* Rightwards iterators *)
 
-val nseq_foldl    : ('a -> 'b -> 'a) -> 'a ->        'b nseq -> 'a
-val nsepseq_foldl : ('a -> 'b -> 'a) -> 'a -> ('b,'c) nsepseq -> 'a
-val sepseq_foldl  : ('a -> 'b -> 'a) -> 'a -> ('b,'c)  sepseq -> 'a
+val nseq_foldl    : ('a -> 'b -> 'a) -> 'a ->           'b nseq -> 'a
+val nsepseq_foldl : ('a -> 'b -> 'a) -> 'a -> ('b,'sep) nsepseq -> 'a
+val sepseq_foldl  : ('a -> 'b -> 'a) -> 'a -> ('b,'sep)  sepseq -> 'a
 
-val nseq_iter    : ('a -> unit) ->        'a nseq -> unit
-val nsepseq_iter : ('a -> unit) -> ('a,'b) nsepseq -> unit
-val sepseq_iter  : ('a -> unit) -> ('a,'b)  sepseq -> unit
+val nsep_or_term_foldl :
+  ('a -> 'b -> 'a) -> 'a -> ('b,'sep) nsep_or_term -> 'a
+
+val sep_or_term_foldl :
+  ('a -> 'b -> 'a) -> 'a -> ('b,'sep) sep_or_term -> 'a
+
+val nsep_or_pref_foldl :
+  ('a -> 'b -> 'a) -> 'a -> ('b,'sep) nsep_or_pref -> 'a
+
+val nseq_iter    : ('a -> unit) ->           'a nseq -> unit
+val nsepseq_iter : ('a -> unit) -> ('a,'sep) nsepseq -> unit
+val sepseq_iter  : ('a -> unit) -> ('a,'sep)  sepseq -> unit
 
 (* Leftwards iterators *)
 
-val nseq_foldr    : ('a -> 'b -> 'b) ->        'a nseq -> 'b -> 'b
-val nsepseq_foldr : ('a -> 'b -> 'b) -> ('a,'c) nsepseq -> 'b -> 'b
-val sepseq_foldr  : ('a -> 'b -> 'b) -> ('a,'c)  sepseq -> 'b -> 'b
+val nseq_foldr    : ('a -> 'b -> 'b) ->           'a nseq -> 'b -> 'b
+val nsepseq_foldr : ('a -> 'b -> 'b) -> ('a,'sep) nsepseq -> 'b -> 'b
+val sepseq_foldr  : ('a -> 'b -> 'b) -> ('a,'sep)  sepseq -> 'b -> 'b
+
+val nsep_or_term_foldr : ('a -> 'b -> 'b) -> ('a,'sep) nsep_or_term -> 'b -> 'b
+val sep_or_term_foldr  : ('a -> 'b -> 'b) -> ('a,'sep)  sep_or_term -> 'b -> 'b
+val nsep_or_pref_foldr : ('a -> 'b -> 'b) -> ('a,'sep) nsep_or_pref -> 'b -> 'b
+
+val nsep_or_term_iter : ('a -> unit) -> ('a,'sep) nsep_or_term -> unit
+val sep_or_term_iter  : ('a -> unit) -> ('a,'sep)  sep_or_term -> unit
+val nsep_or_pref_iter : ('a -> unit) -> ('a,'sep) nsep_or_pref -> unit
 
 (* Maps *)
 
 val nseq_map    : ('a -> 'b) -> 'a nseq -> 'b nseq
-val nsepseq_map : ('a -> 'b) -> ('a,'c) nsepseq -> ('b,'c) nsepseq
-val sepseq_map  : ('a -> 'b) -> ('a,'c)  sepseq -> ('b,'c)  sepseq
+val nsepseq_map : ('a -> 'b) -> ('a,'sep) nsepseq -> ('b,'sep) nsepseq
+val sepseq_map  : ('a -> 'b) -> ('a,'sep)  sepseq -> ('b,'sep)  sepseq
+
+val nsep_or_term_map :
+  ('a -> 'b) -> ('a,'sep) nsep_or_term -> ('b,'sep) nsep_or_term
+
+val sep_or_term_map  :
+  ('a -> 'b) -> ('a,'sep)  sep_or_term -> ('b,'sep) sep_or_term
+
+val nsep_or_pref_map :
+  ('a -> 'b) -> ('a,'sep) nsep_or_pref -> ('b,'sep) nsep_or_pref
+
+(* Conversions to non-empty lists *)
+
+val nsepseq_to_nseq      : ('a,'sep) nsepseq      -> 'a nseq
+val nsep_or_term_to_nseq : ('a,'sep) nsep_or_term -> 'a nseq
+val nsep_or_pref_to_nseq : ('a,'sep) nsep_or_pref -> 'a nseq
+
+val nseq_to_nsepseq : 'sep -> 'a nseq -> ('a,'sep) nsepseq
 
 (* Conversions to lists *)
 
 val nseq_to_list    :        'a nseq -> 'a list
 val nsepseq_to_list : ('a,'b) nsepseq -> 'a list
 val sepseq_to_list  : ('a,'b)  sepseq -> 'a list
+
+val nsep_or_term_to_list : ('a,'sep) nsep_or_term -> 'a list
+val sep_or_term_to_list  : ('a,'sep)  sep_or_term -> 'a list
+val nsep_or_pref_to_list : ('a,'sep) nsep_or_pref -> 'a list
+
+(* Map and concatenate lists *)
+
+val nseq_concat_map    : ('a -> 'b list) ->           'a nseq -> 'b list
+val nsepseq_concat_map : ('a -> 'b list) -> ('a,'sep) nsepseq -> 'b list
+val sepseq_concat_map  : ('a -> 'b list) -> ('a,'sep)  sepseq -> 'b list
+
+(* Conversions from lists *)
+
+val sep_or_term_of_list:
+  'sep ->
+  [< `Sep | `Term ] ->
+  'a list ->
+  ('a,'sep) sep_or_term
+
+val list_to_sepseq : 'a list -> 's -> ('a, 's) sepseq
+
+(* Conversions to JSON *)
+
+type json = Yojson.Safe.t
+
+val yojson_of_nseq : ('a -> json) -> 'a nseq -> json
+
+val yojson_of_nsepseq :
+  ('a -> json) -> ('sep -> json) -> ('a,'sep) nsepseq -> json
+
+val yojson_of_sepseq :
+  ('a -> json) -> ('sep -> json) -> ('a,'sep) sepseq -> json
+
+val yojson_of_sep_or_term :
+  ('a -> json) -> ('sep -> json) -> ('a,'sep) sep_or_term -> json
+
+val yojson_of_nsep_or_term :
+  ('a -> json) -> ('sep -> json) -> ('a,'sep) nsep_or_term -> json
+
+val yojson_of_nsep_or_pref :
+  ('a -> json) -> ('sep -> json) -> ('a,'sep) nsep_or_pref -> json
